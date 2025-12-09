5bf8727212d54b63053d0c19d518a73dcc27fd62
jpmorillom1 <morillojuanpablo2003@gmail.com>
2025-12-08 19:38:12 -0500

    fix(searchbar):remove icons unnesesary

--- src/InputBase.jsx
+++ src/InputBase.jsx
@@ -12,9 +12,6 @@ export default function CustomizedInputBase() {
       component="form"
       sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
     >
-      <IconButton sx={{ p: "10px" }} aria-label="menu">
-        <MenuIcon />
-      </IconButton>
       <InputBase
         sx={{ ml: 1, flex: 1 }}
         placeholder="Search item"
@@ -24,9 +21,6 @@ export default function CustomizedInputBase() {
         <SearchIcon />
       </IconButton>
       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton color="primary" sx={{ p: "10px" }} aria-label="directions">
-        <DirectionsIcon />
-      </IconButton>
     </Paper>
   );
 }
