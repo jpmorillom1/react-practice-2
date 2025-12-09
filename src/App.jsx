d8fd1d944b787cfda813b2ff80ba1ed0ac5ac934
jpmorillom1 <morillojuanpablo2003@gmail.com>
2025-12-08 19:33:59 -0500

    feat(seachbar): add searchbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import DataTable from "./DataTable.jsx";
 import EnhancedTable from "./EnhancedTable.jsx";
+import InitialFilters from "./InitialFilters.jsx";
+import InputBase from "./InputBase.jsx";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -9,7 +11,9 @@ function App() {
     <>
       <DataTable />
       <br></br>
-      <EnhancedTable />;
+      <EnhancedTable />;<br></br>
+      <InputBase />
+      <InitialFilters />
     </>
   );
 }
--- src/EnhancedTable.jsx
+++ src/EnhancedTable.jsx
@@ -364,10 +364,6 @@ export default function EnhancedTable() {
           onRowsPerPageChange={handleChangeRowsPerPage}
         />
       </Paper>
-      <FormControlLabel
-        control={<Switch checked={dense} onChange={handleChangeDense} />}
-        label="Dense padding"
-      />
     </Box>
   );
 }
